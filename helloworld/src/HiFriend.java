7cf7c9a47f53a721edff67d6c3c7d3fe56ec330a
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-05-02 09:14:41 -0400

    stuff

---
+++ helloworld/src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String hiBuddy = "Hi " + "Mark ";
+        System.out.println(hiBuddy);
+    }
+}
---
+++ helloworld/src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I'd like to visit");
+        System.out.println("Maldives");
+        System.out.println("Antarctica");
+    }
+}
